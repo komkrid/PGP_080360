ccfb789a025578c1691cd5fa787c261c202240cf
komkrid <game4670@gmail.com>
2017-03-21 18:30:51 +0700

    test

--- app/src/main/java/anuson/komkid/permitgeographypro/Score_Star.java
+++ app/src/main/java/anuson/komkid/permitgeographypro/Score_Star.java
@@ -1,5 +1,9 @@
 package anuson.komkid.permitgeographypro;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,13 +13,31 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class Score_Star extends AppCompatActivity {
 
     private Button btnSubmit;
     private TextView txtRatingValue,mem_nameTextView;
     private RatingBar ratingBar;
+    private String ratingString;
 
     private String mem_u_idStrings,post_idStrings,mem_idStrings,mem_nameStrings;
+
+    private boolean aBoolean = true;
+    private Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +52,10 @@ public class Score_Star extends AppCompatActivity {
         mem_idStrings = getIntent().getStringExtra("mem_id");
         mem_nameStrings = getIntent().getStringExtra("mem_name");
 
-        Log.d("19MerV5", "post_id_to_user ==>" + mem_u_idStrings);
-        Log.d("19MerV5", "post_id_to_user ==>" + post_idStrings);
-        Log.d("19MerV5", "post_id_to_user ==>" + mem_idStrings);
-        Log.d("19MerV5", "post_id_to_user ==>" + mem_nameStrings);
-
 
         mem_nameTextView = (TextView) findViewById(R.id.textView116);
-        mem_nameTextView.setText("คุณ "+mem_nameStrings);
+        mem_nameTextView.setText("คุณ " + mem_nameStrings);
+
     }//onCreate
 
     private void addListenerOnButton() {
@@ -58,12 +76,80 @@ public class Score_Star extends AppCompatActivity {
         ratingBar = (RatingBar) findViewById(R.id.ratingBar2);
         btnSubmit = (Button) findViewById(R.id.button14);
 
+
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(Score_Star.this,String.valueOf(ratingBar.getRating()),Toast.LENGTH_SHORT).show();
+
+                confirmData();
+
+//              Toast.makeText(Score_Star.this,String.valueOf(ratingBar.getRating()), Toast.LENGTH_SHORT).show();
+
+//                try {
+//                    Syn_user_score syn_user_score = new Syn_user_score(Score_Star.this, mem_u_idStrings);
+//                    syn_user_score.execute();
+//                    String s = syn_user_score.get();
+//                    Log.d("20MerV2", "user_score ==>" + s);
+//
+//                    JSONArray jsonArray = new JSONArray(s);
+//                    for (int i=0;i<jsonArray.length();i+=1){
+//                        JSONObject jsonObject= jsonArray.getJSONObject(i);
+//                        if (post_idStrings.equals(jsonObject.getString("post_id"))) {
+//
+//                            aBoolean = false; //User ซ้ำ
+//
+//                        }//if
+//                    }//for
+//                    if (aBoolean) {
+//                        //User OK
+//                        confirmData();
+//                    }else {
+//                        //User Not OK
+//                        MyAlert myAlert = new MyAlert();
+//                        myAlert.myDialog(context,"User ซ้ำ",
+//                                "กรุณาเปลี่ยน ชื่อผู้ใช้งาน");
+//
+//                    }
+//
+//                }catch (Exception e){
+//                    Log.d("21decV2","e ==>" + e.toString());
+//                }//try
+
+
+            }
+        });//ButtonSubmit
+    }//addListenerOnRatingBar
+
+
+
+    private void confirmData() {
+        ratingString = String.valueOf(ratingBar.getRating());
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.dule_icon);
+        builder.setTitle("โปรดรวจสอบข้อมูล");
+        builder.setMessage("คุณได้ให้" + ratingString + "คะแนน" +"\n");
+        //โชวข้อมูลการกรอก การแจ้งเตือนครั้งสุดท้าย
+
+        builder.setNegativeButton("ยกเลิก", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
             }
         });
+        builder.setPositiveButton("ตกลง", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
 
+//                Add_Score_Star add_score_star = new Add_Score_Star(Score_Star.this,ratingString,post_idStrings,mem_idStrings,mem_u_idStrings);
+//                add_score_star.execute();
+
+
+                dialog.dismiss();
+            }
+        });
+        builder.show();
     }
 }
+
--- app/src/main/java/anuson/komkid/permitgeographypro/Syn_user_score.java
+++ app/src/main/java/anuson/komkid/permitgeographypro/Syn_user_score.java
@@ -1,5 +1,6 @@
 package anuson.komkid.permitgeographypro;
 
+
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
@@ -10,45 +11,34 @@ import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
-
-public class AddScoreToServer extends AsyncTask<Void, Void, String>{
+public class Syn_user_score extends AsyncTask<Void,Void,String>{
 
     private Context context;
-    private static final String urlPHP = "http://swiftcodingthai.com/gam/php_add_score.php";
-    private String score_date, post_id, mem_u_id;
+    private static final String urlJSON = "http://swiftcodingthai.com/gam/get_scorev_star_where_id_user.php";
+    private String men_u_idStrings;
 
-    public AddScoreToServer(Context context,
-                            String score_date,
-                            String post_id,
-                            String mem_u_id) {
+    public Syn_user_score(Context context,
+                          String mem_u_idStrings) {
         this.context = context;
-        this.score_date = score_date;
-        this.post_id = post_id;
-        this.mem_u_id = mem_u_id;
+        this.men_u_idStrings = mem_u_idStrings;
     }
-
     @Override
     protected String doInBackground(Void... voids) {
-
         try {
 
             OkHttpClient okHttpClient = new OkHttpClient();
             RequestBody requestBody = new FormEncodingBuilder()
                     .add("isAdd", "true")
-                    .add("score_date", score_date)
-                    .add("post_id", post_id)
-                    .add("mem_u_id", mem_u_id)
+                    .add("mem_u_id", men_u_idStrings)
                     .build();
             Request.Builder builder = new Request.Builder();
-            Request request = builder.url(urlPHP).post(requestBody).build();
+            Request request = builder.url(urlJSON).post(requestBody).build();
             Response response = okHttpClient.newCall(request).execute();
             return response.body().string();
 
         } catch (Exception e) {
-            Log.d("2febV4", "e doin ==> " + e.toString());
+            Log.d("27novV3", "e doIn ==> " + e.toString());
             return null;
         }
-
-
     }
-}   // Main Class
+}
